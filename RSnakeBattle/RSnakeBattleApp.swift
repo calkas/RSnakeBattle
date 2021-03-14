6461183dbb4487cf70c2840dcfacd2d0ecbf4f24
Piotr Rybka <piotr.rybka87@gmail.com>
2021-03-14 12:55:17 +0100

    Initial Commit

---
+++ RSnakeBattle/RSnakeBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSnakeBattleApp.swift
+//  RSnakeBattle
+//
+//  Created by Piotr Rybka on 14/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct RSnakeBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
